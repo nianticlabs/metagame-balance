bdf7113b57d77d085ced0fc7075438184196dd21
Simão <DracoStriker>
2019-11-05 18:31:37 +0000

    sh scripts refactoring

--- run_distributed.sh
+++ run_distributed.sh
@@ -1,13 +1,12 @@
 export PYTHONPATH=$(pwd)
 
-thread_number=12
-count=`expr $thread_number - 1`
+process_number=12
+count=$(expr process_number - 1)
 
-for i in `seq 0 $count`
-do
-    python Trainer/Deep/Test/Distributed/TestDistributedDeepGIGAWoLF.py "$i" "$thread_number" &
-    echo python Trainer/Deep/Test/Distributed/TestDistributedDeepGIGAWoLF.py "$i" "$thread_number"
-    sleep 1
+for i in $(seq 0 $count); do
+  python Trainer/Deep/Test/Distributed/TestDistributedDeepGIGAWoLF.py "$i" "$process_number" &
+  echo python Trainer/Deep/Test/Distributed/TestDistributedDeepGIGAWoLF.py "$i" "$process_number"
+  sleep 1
 done
 wait
-echo Dcd Mode
+echo Done!
