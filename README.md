fee6f071758f926fa2936470425a0a0005ea593e
Simão Reis <dracostriker@hotmail.com>
2022-01-10 18:26:55 +0000

    started last phase of meta game balance

--- README.md
+++ README.md
@@ -56,5 +56,5 @@ Please cite this work if used.
 
 ## TODO
 
-* Meta-Balance Evaluation (ongoing)
+* Meta-Balance Evaluation and Integration (ongoing)
 * Add Baseline Agents
--- framework/ecosystem/GameBalanceEcosystem.py
+++ framework/ecosystem/GameBalanceEcosystem.py
@@ -1,25 +1,38 @@
+from typing import List
+
 from framework.balance.meta import MetaData
-from framework.balance.restriction import DesignConstraints
+from framework.balance.restriction import VGCDesignConstraints
+from framework.competition import CompetitorManager
 from framework.competition.Competition import Competitor
 from framework.datatypes.Constants import DEFAULT_MATCH_N_BATTLES
-from framework.datatypes.Objects import PkmRoster
+from framework.datatypes.Objects import PkmRoster, get_pkm_roster_view
 from framework.ecosystem.BattleEcosystem import Strategy
 from framework.ecosystem.ChampionshipEcosystem import ChampionshipEcosystem
 
 
 class GameBalanceEcosystem:
 
-    def __init__(self, c: Competitor, battle_c: Competitor, constraints: DesignConstraints, base_roster: PkmRoster,
-                 meta_data: MetaData, debug=False, render=True, n_battles=DEFAULT_MATCH_N_BATTLES,
-                 n_competitors: int = 16):
-        self.c = c
-        self.roster = base_roster
+    def __init__(self, competitor: Competitor, surrogate_agent: List[CompetitorManager],
+                 constraints: VGCDesignConstraints, base_roster: PkmRoster, meta_data: MetaData, debug=False,
+                 render=False, n_battles=DEFAULT_MATCH_N_BATTLES, strategy: Strategy = Strategy.RANDOM_PAIRING):
+        self.c = competitor
         self.constraints = constraints
         self.meta_data = meta_data
-        self.vgc: ChampionshipEcosystem = ChampionshipEcosystem(self.roster, meta_data, debug, render, n_battles)
-        for competitor in range(n_competitors):
-            self.vgc.register(battle_c)
+        self.vgc: ChampionshipEcosystem = ChampionshipEcosystem(base_roster, meta_data, debug, render, n_battles,
+                                                                strategy=strategy)
+        for c in surrogate_agent:
+            self.vgc.register(c)
 
-    def run(self, n_vgc_epochs: int, n_league_epochs: int, strategy: Strategy = Strategy.RANDOM_PAIRING):
-        self.vgc.run(n_vgc_epochs, n_league_epochs, strategy)
-        self.mgb.run()
+    def run(self, n_epochs, n_vgc_epochs: int, n_league_epochs: int):
+        epoch = 0
+        while epoch < n_epochs:
+            self.vgc.run(n_vgc_epochs, n_league_epochs)
+            # TODO evaluate meta
+            # TODO get deltas not entire roster
+            new_roster = self.c.balance_policy.get_action((self.vgc.roster, self.meta_data, self.constraints))
+            violated_rules = self.constraints.check_every_rule(new_roster)
+            if len(violated_rules) == 0:
+                self.vgc.roster = new_roster
+                self.vgc.roster_view = get_pkm_roster_view(self.vgc.roster)
+                # TODO update meta with deltas
+            epoch += 1
