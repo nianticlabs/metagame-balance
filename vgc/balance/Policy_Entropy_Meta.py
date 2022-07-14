e211bc7d9db6ff03e668f841a084f6d2786fb8c7
Sumedh Pendurkar <sumedhpendurkar@nianticlabs.com>
2022-07-14 14:34:42 -0700

    added entropy(P^1)

--- vgc/balance/Policy_Entropy_Meta.py
+++ vgc/balance/Policy_Entropy_Meta.py
@@ -2,16 +2,17 @@ import itertools
 from abc import ABC, abstractmethod
 from math import exp
 from typing import Dict, Tuple, List
-import copy
 from scipy.stats import entropy
+from scipy.special import softmax
 from copy import deepcopy
 #from vgc.util.RosterParsers import MetaRosterStateParser
 from vgc.balance import DeltaRoster
 from vgc.balance.archtype import std_move_dist, std_pkm_dist, std_team_dist
 from vgc.datatypes.Objects import PkmTemplate, PkmMove, PkmFullTeam, PkmRoster
+from vgc.datatypes.Constants import  STAGE_2_STATE_DIM
 from vgc.balance.meta import MetaData, PkmId
 from vgc.util.RosterParsers import MetaRosterStateParser
-
+import numpy as np
 
 class PolicyEntropyMetaData(MetaData):
 
@@ -21,6 +22,7 @@ class PolicyEntropyMetaData(MetaData):
         self._pkm: List[PkmTemplate] = []
 
         self._pkm_wins: Dict[PkmId, int] = {}
+        self.current_policy = None # I don't see another way to do, rather than taking input as P(A_j) as input in evaluate
 
     def set_moves_and_pkm(self, roster: PkmRoster):
         self._pkm = list(roster)
@@ -28,7 +30,7 @@ class PolicyEntropyMetaData(MetaData):
         for pkm in self._pkm:
             self._moves += list(pkm.move_roster)
 
-        init_metadata = copy.deepcopy(self)
+        init_metadata = deepcopy(self)
         self.parser = MetaRosterStateParser(len(self._pkm))
         self.init_state = self.parser.metadata_to_state(init_metadata)
 
@@ -41,16 +43,17 @@ class PolicyEntropyMetaData(MetaData):
 
     def update_metadata(self, **kwargs):
 
-        print('+' * 1000)
-        print(kwargs)
-        self.update_with_delta_roster(kwargs['delta'])
-        self.update_with_policy(kwargs['policy'])
+        if 'delta' in kwargs.keys():
+            self.update_with_delta_roster(kwargs['delta'])
+
+        if 'policy' in kwargs.keys():
+            self.update_with_policy(kwargs['policy'])
         #stage 2 policy
         #delta roster
 
     def update_with_policy(self, policy):
 
-        print("Magic Magic")
+        self.current_policy = policy
 
     def update_with_team(self, team: PkmFullTeam, won: bool):
 
@@ -69,7 +72,17 @@ class PolicyEntropyMetaData(MetaData):
 
 
     def evaluate(self, distance_loss = False) -> float:
-        loss = -entropy([x / sum(self._pkm_wins.values())  for x in self._pkm_wins.values()])
+
+        #TODO: write a function here, so that I don't have to create numpy arrays in object
+        A = np.zeros((len(self._pkm), STAGE_2_STATE_DIM))
+
+        for i, pkm in enumerate(self._pkm):
+            self.current_policy._mark(A[i], [], pkm)
+        u = self.current_policy.get_u_fn()
+        P_A = softmax(u.predict(A))
+
+        print(P_A, P_A.sum())
+        loss = -entropy(P_A)
         if distance_loss:
             return loss + self.distance_from_init_meta()
         return loss
